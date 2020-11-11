d3632beb7ee90f73c3d082d12058912b4880b73b
beastboy0731 <55538371+beastboy0731@users.noreply.github.com>
2020-11-11 14:14:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 function preload() {
-
+    
 }
 
 function setup() {
     canvas=createCanvas(400,400);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(400,400);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses)
 }
+
+function modelLoaded() {
+    console.log('posenet is innitialized');
+}
+
+function gotPoses(results) {
+    if(results.lenght>0) {
+        console.log(results);
+        console.log("nose x="+results[0].pose.nose.x);
+        console.log("nose y="+results[0].pose.nose.y);
+    }
+} 
 
 function draw() {
 
